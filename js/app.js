4454ce1a1c03f0e68daab8303baf5b900a92a95d
Martin <edermartinj@gmail.com>
2025-05-29 16:36:15 -0400

    mi primer commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let ingresosHTML;
+
 let agregarDato = (event) =>{
     event.preventDefault();
     
@@ -8,8 +10,48 @@ let agregarDato = (event) =>{
 if(descripcion!="" && valor !== ""){
     console.log("descipcion: "+ descripcion)
     console.log("valor: "+valor)
-    }else{
+    if(tipo==="ingreso"){
+        cargarIngresos(descripcion, Number(valor));
+
+    }else if (tipo === "egreso"){
+        cargarEgreso(descripcion,Number(valor));
+
+    }
+    document.getElementById("descripcion").value="";
+    document.getElementById("valor")="";
+}else{
         alert("Debe completar todos los campos")
     }
     console.log(tipo)
+}
+
+let cargarIngresos = (descripcion, valor) =>{
+    ingresosHTML = ingresosHTML + crearIngresosHTML(descripcion,valor);
+    totalIngresos=+valor;
+    document.getElementById("ingresoTotal").textContent = totalIngresos
+    document.getElementById("presuouesto").textContent = totalIngresos
+    document.getElementById("lista-ingresos").innerHTML=ingresosHTML;   
+}
+
+
+let crearIngresosHTML=(descripcion,valor)=>{
+    return `<div class="elemento limpiarEstilos">
+                <div class="elemento_descripcion">${descripcion}</div>
+                <div class="derecha limpiarEstilos">
+                    <div class="elemento_valor">${formatearCLP(valor)}</div>
+                    <div class="elemento_eliminar">
+                        <button class="elemento_eliminar--btn">
+                            <ion-icon name="close-circle-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+            </div>`;
+ 
+}
+function formatearCLP(numero) {
+    return new Intl.NumberFormat('es-CL', {
+        style: 'currency',
+        currency: 'CLP',
+        minimumFractionDigits: 0
+    }).format(numero);
 }
