807f9a8a413fb357480a42aaa1a9b0f69588aa19
Martin <edermartinj@gmail.com>
2025-06-04 15:46:03 -0400

    Actualizacion de JS

--- js/app.js
+++ js/app.js
@@ -1,40 +1,46 @@
-let ingresosHTML;
-
+let ingresosHTML="";
+let egresosHTML="";
+let totalIngresos = 0;
+let totalEgresos = 0;
+ 
+ 
 let agregarDato = (event) =>{
     event.preventDefault();
-    
     let tipo = document.getElementById("tipo").value;
     let descripcion = document.getElementById("descripcion").value;
-    let valor = document.getElementById("valor").valor;
-
-if(descripcion!="" && valor !== ""){
-    console.log("descipcion: "+ descripcion)
-    console.log("valor: "+valor)
-    if(tipo==="ingreso"){
-        cargarIngresos(descripcion, Number(valor));
-
-    }else if (tipo === "egreso"){
-        cargarEgreso(descripcion,Number(valor));
-
+    let valor = document.getElementById("valor").value;
+ 
+    if (descripcion!== "" && valor !== "") {
+        console.log("Descripción " + descripcion);
+        console.log("Valor " + valor);
+        if (tipo === "ingreso"){
+            cargarIngresos(descripcion, Number(valor));
+            document.getElementById("descripcion").value = "";
+            document.getElementById("valor").value = "";
+        }else if (tipo === "egreso"){
+            cargarEgresos(descripcion, Number(valor));
+            document.getElementById("descripcion").value = "";
+            document.getElementById("valor").value = "";
+ 
+        }
+ 
+    }else {
+        alert("Completa todos los campos.");
     }
-    document.getElementById("descripcion").value="";
-    document.getElementById("valor")="";
-}else{
-        alert("Debe completar todos los campos")
-    }
-    console.log(tipo)
+    console.log(tipo);
 }
-
-let cargarIngresos = (descripcion, valor) =>{
-    ingresosHTML = ingresosHTML + crearIngresosHTML(descripcion,valor);
-    totalIngresos=+valor;
-    document.getElementById("ingresoTotal").textContent = totalIngresos
-    document.getElementById("presuouesto").textContent = totalIngresos
-    document.getElementById("lista-ingresos").innerHTML=ingresosHTML;   
+ 
+let cargarIngresos = (descripcion, valor) => {
+    ingresosHTML += crearIngresosHTML(descripcion, valor);
+    totalIngresos += valor;
+    document.getElementById("ingresoTotal").textContent = formatearCLP(totalIngresos);
+    document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos - totalEgresos);
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+ 
 }
-
-
-let crearIngresosHTML=(descripcion,valor)=>{
+ 
+ 
+let crearIngresosHTML = (descripcion, valor) => {
     return `<div class="elemento limpiarEstilos">
                 <div class="elemento_descripcion">${descripcion}</div>
                 <div class="derecha limpiarEstilos">
@@ -48,6 +54,32 @@ let crearIngresosHTML=(descripcion,valor)=>{
             </div>`;
  
 }
+ 
+ 
+let cargarEgresos = (descripcion, valor) => {
+    egresosHTML += crearEgresosHTML(descripcion, valor);
+    totalEgresos += valor;
+    document.getElementById("egresoTotal").textContent = formatearCLP(totalEgresos);
+    document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos - totalEgresos);
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+    document.getElementById("calcular_porcentaje").textContent = ((totalEgresos * 100) / totalIngresos).toFixed(1) + "%";
+}
+ 
+let crearEgresosHTML = (descripcion, valor) => {
+    return `<div class="elemento limpiarEstilos">
+                    <div class="elemento_descripcion">${descripcion}</div>
+                    <div class="derecha limpiarEstilos">
+                        <div class="elemento_valor">${formatearCLP(valor)}</div>
+                        <div class="elemento_eliminar">
+                            <button class="elemento_eliminar--btn">
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>`;
+ 
+}
+ 
 function formatearCLP(numero) {
     return new Intl.NumberFormat('es-CL', {
         style: 'currency',
